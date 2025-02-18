04c43558f3fefc51508d173013bbe2f309dbc837
Ikosy <lendres@fifthrace.com>
2025-02-18 15:57:35 -0600

    Update example

--- src/Demo/ViewModels/RecentlyUsedMenuPageViewModel.cs
+++ src/Demo/ViewModels/RecentlyUsedMenuPageViewModel.cs
@@ -10,7 +10,6 @@ public partial class RecentlyUsedMenuPageViewModel : BaseViewModel
 	#region Fields
 
 	private readonly IDialogService     _dialogService;
-	private readonly IMenuService       _menuService;
 
 	#endregion
 
@@ -19,11 +18,17 @@ public partial class RecentlyUsedMenuPageViewModel : BaseViewModel
 	public RecentlyUsedMenuPageViewModel(IDialogService dialogService, IMenuService menuService)
 	{
 		_dialogService      = dialogService;
-		_menuService        = menuService;
 
 		RecentPathsManagerService = new RecentPathsManagerService();
-		RecentPathsManagerService.PushTop(@"C:\Temp\File.txt");
-		RecentPathsManagerService.PushTop(@"C:\Users\Lance\Notes.txt");
+
+		// During the construction of the RecentPathsManagerService, it will restore any saved values.  If
+		// No values were saved, then we will add some here for demonstration purposed (so we don't get a blank
+		// control).  Normally, you would not do this.
+		if (RecentPathsManagerService.GetRecentPaths().Count == 0)
+		{
+			RecentPathsManagerService.PushTop(@"C:\Temp\File.txt");
+			RecentPathsManagerService.PushTop(@"C:\Users\Lance\Notes.txt");
+		}
 	}
 
 	#endregion
@@ -34,65 +39,59 @@ public partial class RecentlyUsedMenuPageViewModel : BaseViewModel
 	[ObservableProperty]
 	public partial IRecentPathsManagerService		RecentPathsManagerService { get; set; }
 
-	[ObservableProperty]
-	public partial bool								CanAddFlyoutItem { get; set; }					= true;
+	//[ObservableProperty]
+	//public partial bool								CanAddNewPath { get; set; }					= true;
 
-	[ObservableProperty]
-	public partial bool								CanAddFlyoutSubItem { get; set; }				= true;
+	//[ObservableProperty]
+	//public partial bool								CanAddFlyoutSubItem { get; set; }				= true;
 
 	public Page? MenuHostingPage
 	{
-		get => _menuService.HostingPage;
-		set
-		{
-			_menuService.HostingPage = value;
-			_dialogService.HostingPage = value;
-		}
+		get => _dialogService.HostingPage;
+		set => _dialogService.HostingPage = value;
 	}
 
 	#endregion
 
 	#region Methods
 
-	private void ShowSelectedMessage(string commandName)
+	private void ShowSelectedMessage(string message)
 	{
-		_dialogService.ShowMessage("MenuFlyoutItem clicked", $"Executing {commandName} showing this message.", "OK");
+		_dialogService.ShowMessage("Menu clicked", $"Executing {message} showing this message.", "OK");
 	}
 
 	[RelayCommand]
-	void AddFlyOutItem()
+	void AddNewPath()
 	{
-		_menuService.AddMenuFlyoutItem("Menu Bar Item", "Added Item", () => ShowSelectedMessage("Added Item"));
-		CanAddFlyoutItem = false;
+		RecentPathsManagerService.PushTop(@"C:\New\New File.txt");
 	}
 
 	[RelayCommand]
-	void RemoveFlyOutItem()
+	void RemoveNewPath()
 	{
-		_menuService.RemoveMenuFlyoutItem("Menu Bar Item", "Added Item");
-		CanAddFlyoutItem = true;
+		RecentPathsManagerService.RemovePath(@"C:\New\New File.txt");
 	}
 
-	[RelayCommand]
-	void AddFlyOutSubItem()
-	{
-		_menuService.AddMenuFlyoutItemToSubMenu("Menu Flyout Sub Item", "Added Sub Item", () => ShowSelectedMessage("Added Sub Item"));
-		CanAddFlyoutSubItem = false;
+	//[RelayCommand]
+	//void AddFlyOutSubItem()
+	//{
+	//	_menuService.AddMenuFlyoutItemToSubMenu("Menu Flyout Sub Item", "Added Sub Item", () => ShowSelectedMessage("Added Sub Item"));
+	//	CanAddFlyoutSubItem = false;
 
-		IMenuFlyoutSubItem menuFlyoutSubItem = _menuService.GetSubMenu("Flyout")!;
-		System.Diagnostics.Debug.WriteLine("");
-		foreach (IMenuElement item in menuFlyoutSubItem)
-		{
-			System.Diagnostics.Debug.WriteLine(item.Text);
-		}
-	}
+	//	IMenuFlyoutSubItem menuFlyoutSubItem = _menuService.GetSubMenu("Flyout")!;
+	//	System.Diagnostics.Debug.WriteLine("");
+	//	foreach (IMenuElement item in menuFlyoutSubItem)
+	//	{
+	//		System.Diagnostics.Debug.WriteLine(item.Text);
+	//	}
+	//}
 
-	[RelayCommand]
-	void RemoveFlyOutSubItem()
-	{
-		_menuService.RemoveMenuFlyoutItemFromSubMenu("Menu Flyout Sub Item", "Added Sub Item");
-		CanAddFlyoutSubItem = true;
-	}
+	//[RelayCommand]
+	//void RemoveFlyOutSubItem()
+	//{
+	//	_menuService.RemoveMenuFlyoutItemFromSubMenu("Menu Flyout Sub Item", "Added Sub Item");
+	//	CanAddFlyoutSubItem = true;
+	//}
 
 	#endregion
 }
